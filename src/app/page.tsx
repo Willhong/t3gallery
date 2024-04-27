466b9084333f569c6344df34e85df7eef90d3804
willhong <hbilly2002@gmail.com>
2024-04-27 18:00:18 +0900

    accually adding database to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,18 @@ import { headers } from "next/headers";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
-const mockUrls = [
-  "https://utfs.io/f/760d5e8f-97a5-450f-b1fe-be33890fc4d2-vaolmp.png",
-  "https://utfs.io/f/bf48cc8e-311a-47b2-8c6d-17d059c9b28a-vaojfi.png",
-  "https://utfs.io/f/88ab73d2-d829-47dd-a727-68c73153dd39-xwupdv.png",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index,
-  url,
-}));
 
 export default async function HomePage() {
   headers();
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  console.log(images);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {[...images, ...images, ...images].map((image, index) => (
           <div key={image.id + "_" + index} className="h-48 w-48">
             <img src={image.url} />
           </div>
