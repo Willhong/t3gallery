3f50d451285075a1c2f23ee20dcee3675225e747
willhong <hbilly2002@gmail.com>
2024-04-27 17:22:13 +0900

    build improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import { index } from "drizzle-orm/mysql-core";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://utfs.io/f/760d5e8f-97a5-450f-b1fe-be33890fc4d2-vaolmp.png",
+  "https://utfs.io/f/bf48cc8e-311a-47b2-8c6d-17d059c9b28a-vaojfi.png",
+  "https://utfs.io/f/88ab73d2-d829-47dd-a727-68c73153dd39-xwupdv.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="h-48 w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       hello gallery work in progress
     </main>
   );
